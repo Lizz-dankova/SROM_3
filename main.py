f296347cc611d0f6aa7633d0b29c076f8803d3a3
Yelizaveta Dankova (Данькова Єлізавета) <elizavettadankoa@gmail.com>
2024-05-23 17:40:32 +0200

    Optimized exponentiation using square and multiply

--- main.py
+++ main.py
@@ -14,58 +14,88 @@ def addition(poly_1, poly_2):
     poly_2 = poly_2.zfill(233)
     c = ['0']
     for i in range(233):
-        c.append(str(((int(poly_1[i]) if i < len(poly_1) else 0) + (int(poly_2[i]) if i < len(poly_2) else 0)) % 2))
+        c.append(str(((int(poly_1[i]) if i < len(poly_1) else 0) ^ (int(poly_2[i]) if i < len(poly_2) else 0))))
     result = ''.join(c)
     return result.lstrip('0') if len(result) <= 233 else result[(len(result) - 233):]
 
 
-def multiply_polynomials(poly_1, poly_2, mod_poly):
-    deg_poly1 = len(poly_1) - 1
-    deg_poly2 = len(poly_2) - 1
-    result = [0] * (deg_poly1 + deg_poly2 + 1)
-    for i in range(deg_poly1 + 1):
-        for j in range(deg_poly2 + 1):
-            result[i + j] ^= int(poly_1[i]) & int(poly_2[j])
-    while result and result[0] == 0:
-        result.pop(0)
-    result = ''.join(map(str, result))
-    result = binary_division(result, int(mod_poly, 2))
+def matrix_create():
+    generator_poly = 0b100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000010010010001
+    multiplicative_matrix = [[0] * 233 for _ in range(233)]
+    for i in range(233):
+        multiplicative_matrix[i][i] = 1  # Identity for simplicity
+
+    # Add generator to the matrix
+    for i in range(1, 233):
+        multiplicative_matrix[0][i] = (generator_poly >> (233 - i - 1)) & 1
+    return multiplicative_matrix
+
+
+def left_shift(number, shift):
+    num_list = list(number)
+    num_list = num_list[shift:] + num_list[:shift]
+    result = ''.join(num_list)
     return result
 
 
-def square(poly_1, mod_poly):
-    sq_ = multiply_polynomials(poly_1, poly_1, mod_poly)
-    return sq_
+def mul(a, b):
+    result = ''
+    multiplicative_matrix = matrix_create()
+
+    a = a.zfill(233)
+    b = b.zfill(233)
+
+    for z in range(233):
+        result_1 = [0 for _ in range(233)]
+        result_2 = 0
+        pre_1 = left_shift(a, z)
+        pre_2 = left_shift(b, z)
+
+        for i in range(233):
+            for j in range(233):
+                result_1[i] += int(pre_1[j]) * multiplicative_matrix[j][i]
+            result_1[i] = result_1[i] % 2
+
+        for i in range(233):
+            result_2 += result_1[i] * int(pre_2[i])
+        result_2 = result_2 % 2
+        result += str(result_2)
+
+    return result.zfill(233)
 
 
-def binary_division(dividend, divisor):
-    quotient_ = 0
-    remainder_ = int(dividend, 2)
-    while remainder_ >= divisor:
-        shift = len(bin(remainder_)) - len(bin(divisor))
-        remainder_ ^= (divisor << shift)
-        quotient_ ^= (1 << shift)
-    remainder_ = bin(remainder_)[2:]
-    while len(remainder_) < 233:
-        remainder_ = remainder_.zfill(233)
-    return remainder_
+def square(bitstring):
+    shift = 1
+    length = len(bitstring)
+    shift %= length
+    shifted_bits = bitstring[-shift:] + bitstring[:-shift]
+    return shifted_bits
 
 
-def power_poly(poly_1, poly_3, mod_poly):
-    result = '1'
+def power(poly_1, poly_3):
+    result = '1'.zfill(233)
     binary_exponent = ''.join(map(str, poly_3))
+    current_power = poly_1
     for bit in binary_exponent[::-1]:
         if bit == '1':
-            result = multiply_polynomials(result, poly_1, mod_poly)
-        poly_1 = square(poly_1, mod_poly)
+            result = mul(result, current_power)
+        current_power = square(current_power)
     return result
 
 
-def reverse(poly_1):
-    power_reverse = 2 ** 233 - 2
-    power_reverse = bin(power_reverse)[2:]
-    power_reverse = str(power_reverse)
-    return power_poly(poly_1, power_reverse, mod)
+def optimized_reverse(poly_1):
+    # Optimized exponentiation using square and multiply
+    result = '1'.zfill(233)
+    base = poly_1
+    exponent = 2 ** 233 - 2
+
+    while exponent > 0:
+        if exponent % 2 == 1:
+            result = mul(result, base)
+        base = square(base)
+        exponent //= 2
+
+    return result
 
 
 def trace(poly):
@@ -75,31 +105,26 @@ def trace(poly):
     return result % 2
 
 
-poly1 = str(input("Enter the first polynom: "))
-poly2 = str(input("Enter the second polynom: "))
-poly3 = str(input("Enter the third polynom: "))
-mod = ('1' + '0' * (233 - 10) + '1000100100001')  # x^233 + x^9 + x^4 + x + 1
+poly1_str = "110101110"
+poly2_str = "101001000"
+poly3_str = '100001101'
 
-
-add_result, add_time = measure_time(addition, poly1, poly2)
+add_result, add_time = measure_time(addition, poly1_str, poly2_str)
 print(f'Addition:  {add_result}')
 print(f'Time taken for Addition: {add_time} seconds')
 
-mul_result, mul_time = measure_time(multiply_polynomials, poly1, poly2, mod)
+mul_result, mul_time = measure_time(mul, poly1_str, poly2_str)
 print(f'Multiply:  {mul_result}')
 print(f'Time taken for Multiply: {mul_time} seconds')
 
-sq_result, sq_time = measure_time(square, poly1, mod)
+sq_result, sq_time = measure_time(square, poly1_str)
 print(f'Square:  {sq_result}')
 print(f'Time taken for Square: {sq_time} seconds')
 
-reverse_result, reverse_time = measure_time(reverse, poly1)
+reverse_result, reverse_time = measure_time(optimized_reverse, poly1_str)
 print(f'Reverse:  {reverse_result}')
 print(f'Time taken for Reverse: {reverse_time} seconds')
 
-pow_result, pow_time = measure_time(power_poly, poly1, poly3, mod)
+pow_result, pow_time = measure_time(power, poly1_str, poly3_str)
 print(f'Power:  {pow_result}')
 print(f'Time taken for Power: {pow_time} seconds')
-
-# trace_result = trace(poly1)
-# print(f'Trace: {trace_result}')
