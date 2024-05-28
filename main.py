a5dca6c235bb40b15c9e577a3795120cdf837115
Yelizaveta Dankova (Данькова Єлізавета) <144330950+Lizz-dankova@users.noreply.github.com>
2024-05-28 15:07:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import time
 
-# Генератор поля (x^233 + x^9 + x^4 + x + 1)
-mod_polynomial = '100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001000010011'
 
 def measure_time(func, *args):
     start_time = time.time()
@@ -10,94 +8,99 @@ def measure_time(func, *args):
     execution_time = end_time - start_time
     return result, execution_time
 
+
 def addition(poly_1, poly_2):
-    poly_1 = poly_1.zfill(233)
-    poly_2 = poly_2.zfill(233)
-    result = ''.join(str(int(a) ^ int(b)) for a, b in zip(poly_1, poly_2))
-    return result.lstrip('0') or '0'
+    poly_1 = poly_1.zfill(173)
+    poly_2 = poly_2.zfill(173)
+    c = ['0']
+    for i in range(173):
+        c.append(str(((int(poly_1[i]) if i < len(poly_1) else 0) + (int(poly_2[i]) if i < len(poly_2) else 0)) % 2))
+    result = ''.join(c)
+    return result.lstrip('0') if len(result) <= 173 else result[(len(result) - 173):]
 
-def left_shift(number, shift):
-    return (number[shift:] + '0' * shift)[:233]
 
-def mod_polynomial_func(dividend, divisor=mod_polynomial):
-    divisor_len = len(divisor)
-    dividend = list(dividend)
-    while len(dividend) >= divisor_len:
-        if dividend[0] == '1':
-            for i in range(divisor_len):
-                dividend[i] = str(int(dividend[i]) ^ int(divisor[i]))
-        dividend.pop(0)
-    return ''.join(dividend).lstrip('0') or '0'
-
-def mul(a, b):
-    a = a.zfill(233)
-    b = b.zfill(233)
-    result = [0] * (2 * 233)
-    for i in range(233):
-        if b[232 - i] == '1':
-            for j in range(233):
-                result[i + j] ^= int(a[232 - j])
+def multiply_polynomials(poly_1, poly_2, mod_poly):
+    deg_poly1 = len(poly_1) - 1
+    deg_poly2 = len(poly_2) - 1
+    result = [0] * (deg_poly1 + deg_poly2 + 1)
+    for i in range(deg_poly1 + 1):
+        for j in range(deg_poly2 + 1):
+            result[i + j] ^= int(poly_1[i]) & int(poly_2[j])
+    while result and result[0] == 0:
+        result.pop(0)
     result = ''.join(map(str, result))
-    return mod_polynomial_func(result, mod_polynomial)
-
-def square(bitstring):
-    interleaved = ''.join(bit + '0' for bit in bitstring[:-1]) + bitstring[-1]
-    return mod_polynomial_func(interleaved, mod_polynomial)
-
-def power(poly_1, poly_3):
-    result = '1'.zfill(233)
-    current_power = poly_1
-    for bit in poly_3:
-        if bit == '1':
-            result = mul(result, current_power)
-        current_power = square(current_power)
+    result = binary_division(result, int(mod_poly, 2))
     return result
 
-def extended_gcd(a, b):
-    if b == 0:
-        return a, 1, 0
-    else:
-        q, r = divmod(a, b)
-        gcd, x1, y1 = extended_gcd(b, r)
-        x = y1
-        y = x1 - q * y1
-        return gcd, x, y
 
-def reverse(poly):
-    poly_int = int(poly, 2)
-    mod_int = int(mod_polynomial, 2)
-    gcd, inv, _ = extended_gcd(mod_int, poly_int)
-    if gcd != 1:
-        raise ValueError("Inverse does not exist")
-    inv_bin = bin(inv)[2:].zfill(233)
-    return inv_bin
+def square(poly_1, mod_poly):
+    sq_ = multiply_polynomials(poly_1, poly_1, mod_poly)
+    return sq_
+
+
+def binary_division(dividend, divisor):
+    quotient_ = 0
+    remainder_ = int(dividend, 2)
+    while remainder_ >= divisor:
+        shift = len(bin(remainder_)) - len(bin(divisor))
+        remainder_ ^= (divisor << shift)
+        quotient_ ^= (1 << shift)
+    remainder_ = bin(remainder_)[2:]
+    while len(remainder_) < 173:
+        remainder_ = remainder_.zfill(173)
+    return remainder_
+
+
+def power_poly(poly_1, poly_3, mod_poly):
+    result = '1'
+    binary_exponent = ''.join(map(str, poly_3))
+    for bit in binary_exponent[::-1]:
+        if bit == '1':
+            result = multiply_polynomials(result, poly_1, mod_poly)
+        poly_1 = square(poly_1, mod_poly)
+    return result
+
+
+def reverse(poly_1):
+    power_reverse = 2 ** 173 - 2
+    power_reverse = bin(power_reverse)[2:]
+    power_reverse = str(power_reverse)
+    return power_poly(poly_1, power_reverse, mod)
+
 
 def trace(poly):
-    return str(sum(map(int, poly)) % 2)
+    result = 0
+    for char in poly:
+        result += int(char)
+    return result % 2
 
-poly1 = str(input("Enter the first polynomial: "))
-poly2 = str(input("Enter the second polynomial: "))
-poly3 = str(input("Enter the third polynomial: "))
+
+poly1 = str(input("Enter the first polynom: "))
+poly2 = str(input("Enter the second polynom: "))
+poly3 = str(input("Enter the third polynom: "))
+mod = ('100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000'
+       '000000000000000000000000000000000000000000000000000010000000111')
+
 
 add_result, add_time = measure_time(addition, poly1, poly2)
 print(f'Addition:  {add_result}')
 print(f'Time taken for Addition: {add_time} seconds')
 
-mul_result, mul_time = measure_time(mul, poly1, poly2)
+mul_result, mul_time = measure_time(multiply_polynomials, poly1, poly2, mod)
 print(f'Multiply:  {mul_result}')
 print(f'Time taken for Multiply: {mul_time} seconds')
 
-sq_result, sq_time = measure_time(square, poly1)
+sq_result, sq_time = measure_time(square, poly1, mod)
 print(f'Square:  {sq_result}')
 print(f'Time taken for Square: {sq_time} seconds')
 
-try:
-    reverse_result, reverse_time = measure_time(reverse, poly1)
-    print(f'Reverse:  {reverse_result}')
-    print(f'Time taken for Reverse: {reverse_time} seconds')
-except ValueError as e:
-    print(e)
+reverse_result, reverse_time = measure_time(reverse, poly1)
+print(f'Reverse:  {reverse_result}')
+print(f'Time taken for Reverse: {reverse_time} seconds')
 
-pow_result, pow_time = measure_time(power, poly1, poly3)
+pow_result, pow_time = measure_time(power_poly, poly1, poly3, mod)
 print(f'Power:  {pow_result}')
 print(f'Time taken for Power: {pow_time} seconds')
+
+# trace_result = trace(poly1)
+# print(f'Trace: {trace_result}')
